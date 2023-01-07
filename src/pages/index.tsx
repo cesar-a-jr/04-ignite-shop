5435c528909da08894884edefe0c5f11e3340a97
cesar-a-jr <cesaraugustojunior1234@gmail.com>
2023-01-06 21:06:08 -0300

    Card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,26 @@
-import { styled } from "../styles"
+import Image from "next/image";
+import { HomeContainer, Product } from "../styles/pages/home";
 
-const Button = styled('button',{
-  backgroundColor: '$green500',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
+import camiseta1 from '../assets/camisetas/1.png'
 
-  span:{
-    fontWeight: "bold"
-  },
-
-  '&:hover':{
-    filter: 'brightness(0.8)'
-  }
-
-})
 
 export default function Home() {
   return (
-    
-    <Button>
-      Enviar
-      <span>TESTE</span>
-    </Button>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} alt='' width={520} height={480}/>
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta1} alt='' width={520} height={480}/>
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
