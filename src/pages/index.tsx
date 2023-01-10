58fafc857090ea3c8e91f71f8c163547285a25bc
Cesar_a_jr <60815258+cesar-a-jr@users.noreply.github.com>
2023-01-10 09:03:43 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { HomeContainer, Product } from "../styles/pages/home";
 
 import camiseta1 from '../assets/camisetas/1.png'
-
+import camiseta2 from '../assets/camisetas/2.png'
 
 export default function Home() {
   return (
@@ -15,9 +15,9 @@ export default function Home() {
         </footer>
       </Product>
       <Product>
-        <Image src={camiseta1} alt='' width={520} height={480}/>
+        <Image src={camiseta2} alt='' width={520} height={480}/>
         <footer>
-          <strong>Camiseta X</strong>
+          <strong>Camiseta y</strong>
           <span>R$ 79,90</span>
         </footer>
       </Product>
